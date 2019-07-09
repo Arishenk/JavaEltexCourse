a36849acb1fb63150d7fd654361d44ba2aa1bd81
Arishenk <i_am_a_celebrity@list.ru>
2019-07-09 13:15:36 +0700

    Create pull threads

--- Threads/src/main/java/com/arishenk/Threads.java
+++ Threads/src/main/java/com/arishenk/Threads.java
@@ -1,14 +1,19 @@
 package com.arishenk;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Threads {
     public static void main(String[] args) throws InterruptedException {
         Printer printer = new Printer();
+        Runnable[] run = new Runnable[10];
+        ExecutorService exec = Executors.newSingleThreadScheduledExecutor();
         for (int i = 0; i < 10; i++) {
-            Runnable r = new MyThread(i, printer);
-            Thread t = new Thread(r);
-            t.start();
+            run[i] = new MyThread(i, printer);
+            exec.submit(run[i]);
         }
         Thread.sleep(3000);
+        exec.shutdown();
         System.out.println("a = " + Printer.a);
     }
 }
