0be6e4fc7739fcc378505b0a33eb1db1bc375ca5
Arishenk <i_am_a_celebrity@list.ru>
2019-07-09 13:03:13 +0700

    Add volatile

--- Threads/src/main/java/com/arishenk/Printer.java
+++ Threads/src/main/java/com/arishenk/Printer.java
@@ -3,12 +3,9 @@ package com.arishenk;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Printer {
-    static public Integer a = 0;
+    static public volatile Integer a = 0;
 
     public void increment() {
-        ReentrantLock lock = new ReentrantLock();
-        lock.lock();
         a += 1;
-        lock.unlock();
     }
 }
