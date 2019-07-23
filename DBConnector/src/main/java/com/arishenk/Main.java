67db0319e3f5babd1d1e942927b82b9aba9f8212
Arishenk <arishenk1998@gmail.com>
2019-07-23 11:53:36 +0700

    Use transaction

--- DBConnector/src/main/java/com/arishenk/Main.java
+++ DBConnector/src/main/java/com/arishenk/Main.java
@@ -5,19 +5,43 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         final String DB_URL = "jdbc:mysql://127.0.0.1:3306/USERS";
+
         try {
             Connection connection = DriverManager.getConnection(DB_URL, "arishenk", "qwerty123"); //соединение с БД
             Statement statement = connection.createStatement();
-            statement.executeUpdate("INSERT INTO DEVELOPERS(fio, phone) VALUE ('katya', '666');"); // добавление/удаление/изменение записей
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM DEVELOPERS;"); // получение записей
-            while (resultSet.next()){ // проход по полученным записям
-                String fio = resultSet.getString("fio"); // получение значений полей
-                System.out.println(fio);
+
+            long startTime = System.nanoTime();
+
+            for (int i = 0; i < 1000; i++) {
+                statement.executeUpdate("INSERT INTO EXAMPLE(letter) VALUE ('" + i + "');"); // добавление/удаление/изменение записей
             }
+
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println("Time without transaction: " + estimatedTime + " ns");
+
             connection.close(); // отключение от БД
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, "arishenk", "qwerty123"); //соединение с БД
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+
+            long startTime = System.nanoTime();
+
+            for (int i = 0; i < 1000; i++) {
+                statement.executeUpdate("INSERT INTO EXAMPLE(letter) VALUE ('" + i + "');"); // добавление/удаление/изменение записей
+            }
+
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println("Time with transaction: " + estimatedTime + " ns");
+
+            connection.commit();
+            connection.close(); // отключение от БД
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
