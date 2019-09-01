d0d49eb7bc8d44dcee3e81c61be6d69750d8fcf2
ArinaNedobitkova <arishenk1998@gmail.com>
2019-09-01 16:16:55 +0700

    Add mockMVC test

--- SpringMVC/src/test/java/com/arishenk/test/ControllerTest.java
+++ SpringMVC/src/test/java/com/arishenk/test/ControllerTest.java
@@ -7,6 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,5 +28,24 @@ public class ControllerTest {
     @Test
     public void contextLoads() throws Exception {
         org.assertj.core.api.Assertions.assertThat(controller).isNotNull();
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void getUser() throws Exception {
+        StringBuilder response = new StringBuilder();
+        FileReader fr = new FileReader("src/main/resources/templates/response.html");
+        Scanner inFile = new Scanner(fr);
+        while (inFile.hasNextLine()) {
+            response.append(inFile.nextLine());
+            response.append("\n");
+        }
+        response.deleteCharAt(response.length() - 2);
+        response.deleteCharAt(response.length() - 1);
+        fr.close();
+
+        this.mockMvc.perform(get("http://localhost:8081/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(response.toString())));
     }
 }
